458659fdfab74dbc833fa0eb123d5917bad3a070
arescaldinoch <197246038+arescaldinoch@users.noreply.github.com>
2025-04-03 10:06:59 +0200

    Update zowe_operations.sh test 12

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -13,8 +13,7 @@ else
 fi
 
 # Upload files
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive
---binary-files "cobol-check-0.2.16.jar"
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.16.jar"
 
 # Verify upload
 echo "Verifying upload:"
